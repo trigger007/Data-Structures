2aa819067ab32972a69f687f5301ef4d10332291
trigger007 <aditya1999nov@gmail.com>
2020-12-30 20:43:06 +0530

    stack error, fix req

--- stack.cpp
+++ stack.cpp
@@ -1,9 +1,10 @@
 #include<iostream>
+#include <cstdlib>
 using namespace std;
 
 class node{
     public:
-    int val;
+    int data;
     node * next;
 };
 node *createnode(){
@@ -12,22 +13,40 @@ node *createnode(){
     int n;
     cout<<"enter the number you want to enter:";
     cin>>n;
-    newnode->val=n;
+    newnode->data=n;
     newnode->next=NULL;
     return newnode;
 }
 node *push(node *top,node *newnode){
-    node *newnode;
-    
+    newnode->next=top;
+    top=newnode;
+    return top;
 }
-node *addnum(int n){
-    node *newnode;
-    newnode=createnode();
-
-}
-void main(){
-    int n;
+node *addnum(node *top){
+    int n,i;
     cout<<"enter the number of elements you want to push in the stack";
     cin>>n;
-    addnum(n);
+    node *newnode;
+    for (i=0;i<n;i++){
+        newnode=createnode();
+        top=push(top,newnode);
+        free(newnode);
+    }
+    return top;
+}
+void display(node* top){
+    node *temp;
+    temp=top;
+    while(temp->next!=NULL){
+        cout<<temp->data;
+        temp=temp->next;
+    }
+}
+int main(){
+    node *top;
+    int t;
+    cout<<"Enter the first element in the stack:";
+    cin>>t;
+    top=addnum(top);
+    display(top);
 }
