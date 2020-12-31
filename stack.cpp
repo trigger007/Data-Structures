f5a9693bce005e2c45687eabc0efefc7fd6a22fe
trigger007 <aditya1999nov@gmail.com>
2020-12-31 22:10:22 +0530

    stack functioning

--- stack.cpp
+++ stack.cpp
@@ -41,18 +41,65 @@ node *addnum(node *top){
     //cout<<top->data;
     return top;
 }
+node *add1num(node *top){
+    node *newnode;
+    newnode=createnode();
+    top=push(top,newnode);
+    return top;
+}
 void display(node *top){
     node *temp;
     temp=top;
+    if (top==NULL){
+        cout<<"The stack is empty"<<endl;
+    }
     while(temp!=NULL){
         cout<<temp->data<</*" "<<temp->next<<*/endl;
         temp=temp->next;
     }
 }
+node *pop(node *top){
+    node *temp;
+    temp=top;
+    if (top==NULL){
+        cout<<"The stack is empty"<<endl;
+        return top;
+    }
+    else if(temp->next==NULL){
+        cout<<"Warning! This was the last element in the stack"<<endl;
+    }
+    else if (temp->next->next==NULL){
+        cout<<"Warning! Only one element is left in the stack after this operation."<<endl;
+    }
+    cout<<"element popped is:"<<top->data<<endl;
+    top=top->next;
+    return top;
+}
 int main(){
     node *top;
     top=createnode();
-    display(top);
-    top=addnum(top);
-    display(top);
+    int s;
+    while(s){
+        cout<<"enter 1. to push()."<<endl;
+        cout<<"enter 2. to pop()."<<endl;
+        cout<<"enter 3. to push more than one element."<<endl;
+        cout<<"enter 4. to display stack."<<endl;
+        cout<<"enter your option:";
+        cin>>s;
+        if (s==1){
+            top=add1num(top);
+        }
+        else if(s==2){
+            top=pop(top);
+        }
+        else if(s==3){
+            top=addnum(top);
+        }
+        else if(s==4){
+            display(top);
+        }
+        else{
+            break;
+        }
+    }
 }
