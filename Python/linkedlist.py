3e6a37c9eaf4da05c2774bf59347a41a6decdba5
Aditya Singh <aditya1999nov@gmail.com>
2021-02-25 13:31:08 +0530

    pylinkedlist

--- Python/linkedlist.py
+++ Python/linkedlist.py
@@ -10,18 +10,62 @@ class node:
         a=node(data)
         a.next=None
         return a
-    def display(self,head):
+    def addnum(self,head):
+        val=int(input("enter the number you want to add:"))
+        newnode=self.createnode(val)
         temp=head
-        print(temp.val)
         while temp.next!=None:
             temp=temp.next
-            print(temp.val)
+        temp.next=newnode
+        self.display(head)
+    def delpos(self,head,pos):
+        temp=head
+        i=0;
+        for i in range(pos-1):
+            temp=temp.next
+        temp1=temp
+        temp2=temp.next.next
+        temp=temp.next
+        del temp
+        temp1.next=temp2
+
+    def delval(self,head,val):
+        temp=head
+        temp1=temp
+        if head.val==val:
+            temp=head.next
+            del head
+            head=temp
+        else:
+            temp=head.next
+            while temp.val!=val:
+                temp=temp.next
+                temp1=temp1.next
+            temp2=temp.next
+            temp1.next=temp2
+            del temp
+        return head
+        
+        # print("choose the part you want to delete")
+        # print("1. for node by value")
+        # print("2. for node by position")
+        # a=int(input("enter your option:"))
+
+    def display(self,head):
+        temp=head
+        print(temp.val,"->",end =" ")
+        while temp.next!=None:
+            temp=temp.next
+            print(temp.val,"->",end =" ")
+    
             
 head=node(5)
-a=node(6)
-b=node(7)
-head.next=a
-a.next=b
+
+n=int(input("enter number of elements to add:"))
+for i in range(n):
+    head.addnum(head)
+f=int(input("enter the number to be deleted:"))
+head=head.delval(head,f)
 head.display(head)
 
 
